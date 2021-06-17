a869832f175eda63242837ee1ae9d5d0833fd430
haolange <1824339952@qq.com>
2021-06-17 17:15:46 +0800

    [Code]Update

--- Runtime/RenderPipeline/RenderPass/OpaqueDepth.cs
+++ Runtime/RenderPipeline/RenderPass/OpaqueDepth.cs
@@ -48,5 +48,31 @@ namespace InfinityTech.Rendering.Pipeline
                 graphContext.renderContext.DrawRenderers(passData.rendererList.cullingResult, ref passData.rendererList.drawSettings, ref passData.rendererList.filteringSettings);
             });
         }
+
+        /*void RenderOpaqueDepth(Camera camera, FCullingData cullingData, CullingResults cullingResult)
+        {
+            RendererList rendererList = RendererList.Create(CreateRendererListDesc(camera, cullingResult, InfinityPassIDs.OpaqueDepth, new RenderQueueRange(2450, 2999)));
+            TextureDescription depthDescription = new TextureDescription(camera.pixelWidth, camera.pixelHeight) { clearBuffer = true, dimension = TextureDimension.Tex2D, enableMSAA = false, bindTextureMS = false, name = FOpaqueDepthString.TextureName, depthBufferBits = EDepthBits.Depth32 };
+            RDGTextureRef depthTexture = m_GraphBuilder.ScopeTexture(InfinityShaderIDs.DepthBuffer, depthDescription);
+
+            RDGPassBuilder passBuilder = m_GraphBuilder.AddPass<FOpaqueDepthData>(FOpaqueDepthString.PassName, ProfilingSampler.Get(CustomSamplerId.OpaqueDepth));
+
+            passData.rendererList = rendererList;
+            passData.depthBuffer = passBuilder.UseDepthBuffer(depthTexture, EDepthAccess.ReadWrite);
+            m_DepthMeshProcessor.DispatchSetup(ref cullingData, new FMeshPassDesctiption(2450, 2999));
+
+            passBuilder.SetExecuteFunc((ref FOpaqueDepthData passData, ref RDGGraphContext graphContext) =>
+            {
+                //MeshDrawPipeline
+                m_DepthMeshProcessor.DispatchDraw(ref graphContext, 0);
+
+                //UnityDrawPipeline
+                passData.rendererList.drawSettings.sortingSettings = new SortingSettings(camera) { criteria = SortingCriteria.QuantizedFrontToBack };
+                passData.rendererList.drawSettings.enableInstancing = m_RenderPipelineAsset.EnableInstanceBatch;
+                passData.rendererList.drawSettings.enableDynamicBatching = m_RenderPipelineAsset.EnableDynamicBatch;
+                passData.rendererList.filteringSettings.renderQueueRange = new RenderQueueRange(2450, 2999);
+                graphContext.renderContext.DrawRenderers(passData.rendererList.cullingResult, ref passData.rendererList.drawSettings, ref passData.rendererList.filteringSettings);
+            });
+        }*/
     }
 }
