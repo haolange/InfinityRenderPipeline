f2581b7a7c4c25a8f09c29665cbd6c66c7dd9e12
haolange <1824339952@qq.com>
2021-10-12 13:06:11 +0800

    [Code&Shader]Update VEG

--- Editor/Tool/VFXGraph/InfinityRP.VFXBind.cs
+++ Editor/Tool/VFXGraph/InfinityRP.VFXBind.cs
@@ -6,8 +6,8 @@ namespace InfinityTech.Tool.Editor
 {
     class VFXInfinityRPBinder : VFXSRPBinder
     {
-        public override string templatePath { get { return "Packages/com.unity.render-pipelines.infinity/Editor/Tools/VFXGraph/Shaders"; } }
-        public override string runtimePath { get { return "Packages/com.unity.render-pipelines.infinity/Runtime/Tools/VFXGraph/Shaders"; } }
+        public override string templatePath { get { return "Packages/com.infinity.render-pipeline/Editor/Tool/VFXGraph/Shaders"; } }
+        public override string runtimePath { get { return "Packages/com.infinity.render-pipeline/Runtime/Tool/VFXGraph/Shaders"; } }
         public override string SRPAssetTypeStr { get { return typeof(InfinityRenderPipelineAsset).Name; } }
         public override Type SRPOutputDataType { get { return null; } }
     }
